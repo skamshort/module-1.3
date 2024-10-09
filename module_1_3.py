6f527ddb97dddf28d6cce39f42824d959f6354dc
skamshort <andreev16012004@gmail.com>
2024-10-09 13:37:03 +0700

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,14 @@
+name = 'Max'
+print(name, type(name))
+name = 2
+print(name, type(name))
+name = 2.6
+print(name, type(name))
+name = 1, 2, 3
+print(name, type(name))
+age = 20
+new_age = '21'
+print(age, type(age))
+print(new_age, type(new_age))
+is_student = True
+print(is_student, type(is_student))
